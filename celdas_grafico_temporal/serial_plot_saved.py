b1dc39ea4a31f7cc845755051bc12aba40972696
Ulises C <52766555+uwulises@users.noreply.github.com>
2025-11-26 15:09:28 -0300

    update

--- celdas_grafico_temporal/serial_plot_saved.py
+++ celdas_grafico_temporal/serial_plot_saved.py
@@ -7,7 +7,7 @@ import numpy as np
 import json
 import pandas as pd
 # Serial port configuration
-serial_port = '/dev/tty.usbmodem1422301'  # Replace with your Arduino's serial port (e.g., COM3 on Windows)
+serial_port = '/dev/tty.usbmodem1412201'  # Replace with your Arduino's serial port (e.g., COM3 on Windows)
 baud_rate = 57600
 timeout = 5
 
@@ -16,6 +16,8 @@ all_data = []
 plt.ion()
 fig, ax = plt.subplots()
 line_ax, = ax.plot([])
+line_2_ax, = ax.plot([])
+line_3_ax, = ax.plot([])
 ax.set_xlim(0, 100)
 ax.set_title('Celda de carga')
 ax.set_xlabel('Tiempo (s)')
@@ -26,32 +28,20 @@ button = plt.Button(plt.axes([0.8, 0.05, 0.15, 0.075]), 'Save data')
 
 x_data = np.array([])
 y_data = np.array([])
-
-
-# Function to save the data to a CSV file
-def save_to_csv(name='G'):
-    if all_data:
-        now = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"{name}_data_{now}.csv"
-        with open(filename, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['celda_1', 'celda_2', 'celda_3', 'celda_4', 'celda_5', 'celda_6', 'celda_7', 'celda_8', 'celda_9'])
-            writer.writerows(all_data)
-        print(f"\nData saved to '{filename}'")
-    else:
-        print("\nNo data to save.")
-    return filename
+y2_data = np.array([])
+y3_data = np.array([])
 
 def save_data(event):
     # example data
     datos_tiempo = x_data
-    datos_carga = y_data
+    datos_celda_1 = y_data
+    datos_celda_2 = y2_data
 
     # create a pandas DataFrame from the data
-    df = pd.DataFrame({'Tiempo': datos_tiempo, 'Carga': datos_carga})
+    df = pd.DataFrame({'Tiempo': datos_tiempo, 'Celda_1': datos_celda_1, 'Celda_2': datos_celda_2, 'Celda_3': y3_data})
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     # save the DataFrame to a CSV file
-    df.to_csv('datos_celda_{}.csv'.format(timestamp), index=False)
+    df.to_csv('datos_celdas_{}.csv'.format(timestamp), index=False)
     print('Data saved')
     exit()
 
@@ -85,16 +75,15 @@ try:
             celda_7 = float(data_dict["celda_7"])
             celda_8 = float(data_dict["celda_8"])
             celda_9 = float(data_dict["celda_9"])
-
-            # Add the data to the list for saving to CSV
-            all_data.append([celda_1, celda_2, celda_3, celda_4, celda_5, celda_6, celda_7, celda_8, celda_9])
-            #print(f"Celda 1: {celda_1}, Celda 2: {celda_2}, Celda 3: {celda_3}, Celda 4: {celda_4}, Celda 5: {celda_5}, Celda 6: {celda_6}, Celda 7: {celda_7}, Celda 8: {celda_8}, Celda 9: {celda_9}")
-
             x_data = np.append(x_data, time.time() - start_time)
             y_data = np.append(y_data, celda_1)
+            y2_data = np.append(y2_data, celda_2)
+            y3_data = np.append(y3_data, celda_3)
             line_ax.set_data(x_data, y_data)
+            line_2_ax.set_data(x_data, y2_data)
+            line_3_ax.set_data(x_data, y3_data)
             ax.set_xlim(max(0, x_data[-1] - 100), x_data[-1])
-            ax.set_ylim(min(y_data)-100, max(y_data)+100)
+            ax.set_ylim(min(y_data)*0.9, max(y_data)*1.1)
             fig.canvas.draw()
             fig.canvas.flush_events()
 
