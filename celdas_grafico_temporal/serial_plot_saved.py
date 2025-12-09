97a1ece8e2cc183e730cf8dbad743cafc32df671
Ulises C <52766555+uwulises@users.noreply.github.com>
2025-12-09 15:46:48 -0300

    update factor value

--- celdas_grafico_temporal/serial_plot_saved.py
+++ celdas_grafico_temporal/serial_plot_saved.py
@@ -73,7 +73,6 @@ try:
     time.sleep(2)  # Wait for the serial port to initialize
     ser.flush()
     start_time = time.time()
-    last_time = time.time()
     while True:  
         line = ser.readline().decode('utf-8', errors='ignore').strip()
         if not line:
@@ -83,14 +82,14 @@ try:
                 # Parse the JSON data
                 data_dict = json.loads(line)
                 # Extract the values from the JSON data
-                celda_1 = float(data_dict["celda_1"])
-                celda_2 = float(data_dict["celda_2"])
-                celda_3 = float(data_dict["celda_3"])
+                celda_1 = (float(data_dict["celda_1"])-234500)/823500
+                celda_2 = (float(data_dict["celda_2"])-314500)/855500
+                celda_3 = (float(data_dict["celda_3"])-184000)/832500
                 celda_4 = float(data_dict["celda_4"])
                 celda_5 = float(data_dict["celda_5"])
                 celda_6 = float(data_dict["celda_6"])
                 celda_7 = float(data_dict["celda_7"])
-                celda_8 = float(data_dict["celda_8"])
+                celda_8 = (float(data_dict["celda_8"])- 97500)/859500
                 celda_9 = float(data_dict["celda_9"])
                 x_data = np.append(x_data, time.time() - start_time)
                 y1_data = np.append(y1_data, celda_1)
@@ -102,23 +101,20 @@ try:
                 y7_data = np.append(y7_data, celda_7)
                 y8_data = np.append(y8_data, celda_8)
                 y9_data = np.append(y9_data, celda_9)
-                #if one second has passed, update the plot
-                if (time.time() - last_time )>= 0.5:
-                    line_ax.set_data(x_data, y1_data)
-                    line_2_ax.set_data(x_data, y2_data)
-                    line_3_ax.set_data(x_data, y3_data)
-                    line_4_ax.set_data(x_data, y4_data)
-                    line_5_ax.set_data(x_data, y5_data)
-                    line_6_ax.set_data(x_data, y6_data)
-                    line_7_ax.set_data(x_data, y7_data)
-                    line_8_ax.set_data(x_data, y8_data)
-                    line_9_ax.set_data(x_data, y9_data)
-                    fig.canvas.draw()
-                    fig.canvas.flush_events()
-                    ax.set_xlim(max(0, x_data[-1] - 100), x_data[-1])
-                    ax.set_ylim(min(np.min(y1_data), np.min(y2_data), np.min(y3_data), np.min(y4_data), np.min(y5_data), np.min(y6_data), np.min(y7_data), np.min(y8_data), np.min(y9_data))* 0.9,
-                                 max(np.max(y1_data), np.max(y2_data), np.max(y3_data), np.max(y4_data), np.max(y5_data), np.max(y6_data), np.max(y7_data), np.max(y8_data), np.max(y9_data))* 1.1)
-                    last_time = time.time()
+                line_ax.set_data(x_data, y1_data)
+                line_2_ax.set_data(x_data, y2_data)
+                line_3_ax.set_data(x_data, y3_data)
+                line_4_ax.set_data(x_data, y4_data)
+                line_5_ax.set_data(x_data, y5_data)
+                line_6_ax.set_data(x_data, y6_data)
+                line_7_ax.set_data(x_data, y7_data)
+                line_8_ax.set_data(x_data, y8_data)
+                line_9_ax.set_data(x_data, y9_data)
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+                ax.set_xlim(max(0, x_data[-1] - 100), x_data[-1])
+                ax.set_ylim(min(np.min(y1_data), np.min(y2_data), np.min(y3_data), np.min(y4_data), np.min(y5_data), np.min(y6_data), np.min(y7_data), np.min(y8_data), np.min(y9_data)) - 0.1,
+                             max(np.max(y1_data), np.max(y2_data), np.max(y3_data), np.max(y4_data), np.max(y5_data), np.max(y6_data), np.max(y7_data), np.max(y8_data), np.max(y9_data)) + 0.1)
             except json.JSONDecodeError as e:
                 print(e)
                 continue  # Skip invalid JSON
